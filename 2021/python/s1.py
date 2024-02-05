66340b6bc89ca516fba13b9dd54a2dc473939c86
Aarav <43528744+Aarav87@users.noreply.github.com>
2024-02-04 20:46:06 -0500

    restructured repo

--- 2021/python/s1.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-heights = list(map(int, input().split()))
-widths = list(map(int, input().split()))
-
-area = 0
-for i in range(n):
-    area += widths[i] * (heights[i] + heights[i+1]) / 2
-
-print(area)
